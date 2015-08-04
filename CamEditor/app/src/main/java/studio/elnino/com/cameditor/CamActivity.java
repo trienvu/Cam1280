2daace013251245f6cf554d5fd97498fd8209c1e
Trien_vudinh <trien.vudinh@gemtek.com.vn>
2015-08-05 00:11:37 +0700

    Adding simple call sdks function

--- CamEditor/app/src/main/java/studio/elnino/com/cameditor/CamActivity.java
+++ CamEditor/app/src/main/java/studio/elnino/com/cameditor/CamActivity.java
@@ -1,9 +1,16 @@
 package studio.elnino.com.cameditor;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.aviary.android.feather.sdk.AviaryIntent;
+import com.aviary.android.feather.sdk.internal.Constants;
+import com.aviary.android.feather.sdk.internal.headless.utils.MegaPixels;
 
 
 public class CamActivity extends ActionBarActivity {
@@ -12,6 +19,38 @@ public class CamActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
+
+        Intent intent = AviaryIntent.createCdsInitIntent(getBaseContext());
+        startService(intent);
+    }
+
+    private void goEditPage() {
+        Intent newIntent = new AviaryIntent.Builder(this)
+                .setData(uri) // input image src
+                .withOutput(Uri.parse("file://" + mOutputFilePath)) // output file
+                .withOutputFormat(Bitmap.CompressFormat.JPEG) // output format
+                .withOutputSize(MegaPixels.Mp5) // output size
+                .withOutputQuality(90) // output quality
+                .build();
+
+        // start the activity
+        startActivityForResult(newIntent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 1: // this is the request code we used in this example
+                    Uri mImageUri = data.getData(); // generated output file
+                    Bundle extra = data.getExtras();
+                    if (null != extra) {
+                        // image has been changed?
+                        boolean changed = extra.getBoolean(Constants.EXTRA_OUT_BITMAP_CHANGED);
+                    }
+                    break;
+            }
+        }
     }
 
     @Override
