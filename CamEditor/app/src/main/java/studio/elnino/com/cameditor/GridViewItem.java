fb075d21ce400dc665d819dd1d9b8f5fb2b7fbd2
Trienvu <Trienvu135@gmail.com>
2015-08-30 23:28:56 +0700

    Add picasso lib and create new gallery

---
+++ CamEditor/app/src/main/java/studio/elnino/com/cameditor/GridViewItem.java
@@ -0,0 +1,35 @@
+package studio.elnino.com.cameditor;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by trien on 8/30/2015.
+ */
+public class GridViewItem {
+
+    private String path;
+    private boolean isDirectory;
+    private Bitmap image;
+
+
+    public GridViewItem(String path, boolean isDirectory, Bitmap image) {
+        this.path = path;
+        this.isDirectory = isDirectory;
+        this.image = image;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+
+    public Bitmap getImage() {
+        return image;
+    }
+}
